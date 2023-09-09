bae9b7455f474511ba55971ad48dfd39f84da6e4
ArthurRL <arthurrlima29@gmail.com>
2023-09-09 01:50:12 -0300

    Correções no script.js

--- script.js
+++ script.js
@@ -1,98 +1,78 @@
-/* correct = true;
-let path = "";
+const formLogin = document.getElementById("login");
+console.log(formLogin)
 
-const form = document.querySelectorAll("form");
-const destino = document.getElementById("destino").addEventListener("change", (e) => {
-	destinyPage = e.target.value;
-});
 
-form[0].addEventListener("submit",
-	(e) => { 
-		e.preventDefault();
-		console.log(e.target);
-		const username = e.target.username.value;
-		const password = e.target.password.value;
-		const destino = e.target.destino;
-		console.log(username);
-		console.log(password);
-		console.log(destino);
-		
-		if (username != "") {
-			isValid = /{4, 20}/.test(username);
-			if (isValid) { correct = true } 
-			else { 
-				correct = false;
-				alert("Incorrect username format.");
-			}
-		} else { 
-			correct = false;
-			alert("Username is empty.");
-		}
+formLogin.addEventListener("submit", validateLogin);
+console.log(formLogin);
 
-		if (password != "") {
-			isValid = /[a-zA-Z0-9]{6, 10}/.test(password);
-			if (isValid) {
-				correct == true;
-			} else {
-				alert("Incorrect password format.");
-				correct = false;
-			}
-		} else { 
-			alert("Incorrect password format.");
-			correct = false;
-		}
+function validateLogin(e) {
+	e.preventDefault();
+	const email = e.target.email.value;
+	const password = e.target.password.value;
+	isValid = (/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/.test(password)) && (password.length >= 6 && password.length <= 10);
+	console.log(isValid, password.length);
 
-		if (correct) {
-			if (destinyPage != "Carrinho de Compras") {
-				window.location.href = "./support/support.html"
-			} else { window.location.href = "./cart/cart.html" }
-		}
+	if (isValid) { window.location.href = "./cart/cart.html" } else { return alert("Formato inválido de senha.") }
+};
 
-	});
+const formRegister = document.getElementById("register");
+console.log(formRegister)
 
-console.log(form);
 
-function valida(form) {
-	
-}; */
+formRegister.addEventListener("submit", validateRegister);
+console.log(formRegister);
+
+function validateRegister(e) {
+	e.preventDefault();
+	const nome = e.target.name.value;
+	const sobrenome = e.target.sobrenome.value;
+	const email = e.target.email.value;
+	const password = e.target.password.value;
+	isValid = (/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/.test(password)) && (password.length >= 6 && password.length <= 10);
+	nomeLengthIsValid = nome.length >= 2 && sobrenome.length >= 2; 
+	console.log(isValid, nomeLengthIsValid, password.length);
+
+	if (isValid && nomeLengthIsValid) { window.location.href = "./cart/cart.html" } else { return alert("Formato inválido de nome, ou de senha.") }
+};
+
 
 
 /*
 
-	else if (form.destino.selectedIndex==0) {
+	else if (formLogin.destino.selectedIndex==0) {
 		alert("Destino not selected.");
 		correct = false;
 */
 
 function myMenuFunction() {
-    var i = document.getElementById("navMenu");
+	var i = document.getElementById("navMenu");
 
-    if(i.className === "nav-menu") {
-        i.className += " responsive";
-    } else {
-        i.className = "nav-menu";
-    }
-   }
+	if(i.className === "nav-menu") {
+	    i.className += " responsive";
+	} else {
+	    i.className = "nav-menu";
+	}
+}
 
-   var a = document.getElementById("loginBtn");
-    var b = document.getElementById("registerBtn");
-    var x = document.getElementById("login");
-    var y = document.getElementById("register");
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
 
-    function login() {
-        x.style.left = "4px";
-        y.style.right = "-520px";
-        a.className += " white-btn";
-        b.className = "btn";
-        x.style.opacity = 1;
-        y.style.opacity = 0;
-    }
+function login() {
+    x.style.left = "4px";
+    y.style.right = "-520px";
+    a.className += " white-btn";
+    b.className = "btn";
+    x.style.opacity = 1;
+    y.style.opacity = 0;
+}
 
-    function register() {
-        x.style.left = "-510px";
-        y.style.right = "5px";
-        a.className = "btn";
-        b.className += " white-btn";
-        x.style.opacity = 0;
-        y.style.opacity = 1;
-    }
+function register() {
+    x.style.left = "-510px";
+    y.style.right = "5px";
+    a.className = "btn";
+    b.className += " white-btn";
+    x.style.opacity = 0;
+    y.style.opacity = 1;
+}
