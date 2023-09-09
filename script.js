099c97125bc826a25ea0c9dd5196bccee83e62af
LeonardoRdc <leordc_4282@hotmail.com>
2023-09-09 00:42:45 -0300

    arthurgayzao

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-correct = true;
+/* correct = true;
 let path = "";
 
 const form = document.querySelectorAll("form");
@@ -54,7 +54,7 @@ console.log(form);
 
 function valida(form) {
 	
-};
+}; */
 
 
 /*
@@ -63,3 +63,36 @@ function valida(form) {
 		alert("Destino not selected.");
 		correct = false;
 */
+
+function myMenuFunction() {
+    var i = document.getElementById("navMenu");
+
+    if(i.className === "nav-menu") {
+        i.className += " responsive";
+    } else {
+        i.className = "nav-menu";
+    }
+   }
+
+   var a = document.getElementById("loginBtn");
+    var b = document.getElementById("registerBtn");
+    var x = document.getElementById("login");
+    var y = document.getElementById("register");
+
+    function login() {
+        x.style.left = "4px";
+        y.style.right = "-520px";
+        a.className += " white-btn";
+        b.className = "btn";
+        x.style.opacity = 1;
+        y.style.opacity = 0;
+    }
+
+    function register() {
+        x.style.left = "-510px";
+        y.style.right = "5px";
+        a.className = "btn";
+        b.className += " white-btn";
+        x.style.opacity = 0;
+        y.style.opacity = 1;
+    }
